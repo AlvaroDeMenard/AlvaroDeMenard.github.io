b47c3830fccd4d8c7b815a16a74b97c60e904ca3
AlvaroDeMenard <alvarodemenard@gmail.com>
2020-12-04 01:32:35 +0200

    minor fixes

--- js/smartresize.min-30ce71fc8cb3290e9eb11a89721d4f41.js
+++
@@ -1,1 +0,0 @@
-!function(i,t){jQuery.fn[t]=function(i){return i?this.bind("resize",(e=i,function(){var i=this,t=arguments;u&&clearTimeout(u),u=setTimeout(function(){e.apply(i,t),u=null},100)})):this.trigger(t);var e,u}}(jQuery,"smartresize");
